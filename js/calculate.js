1103535588bb4099148f37f2739f815fdf1e6514
Justin Chau <chausicle@gmail.com>
2019-03-24 18:55:31 -0700

    Allow equal button pressed for continuious calculation

--- js/calculate.js
+++ js/calculate.js
@@ -1,4 +1,6 @@
 function calculate(numArray, selectedOperator) {
+  console.log('numArray', numArray);
+  if(numArray[0] === NaN || numArray[1] === NaN) return 'Error';
   switch(selectedOperator) {
     case 'divide':
       if(numArray[1] === 0) return 'Error';
--- js/index.js
+++ js/index.js
@@ -3,71 +3,92 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function startCalculator() {
-  let screen = document.querySelector('.screen');
-  let numArray = [0, 0];
+  let screen = document.querySelector(".screen");
+  let numArray = [NaN, NaN];
   let decimalPressed = false; // to determine the first decimal pressed to prevent multiple decimals appended on a single number
   let operatorPressed = false;
+  let calculated = false;
   let selectedOperator;
 
-  document.addEventListener('click', function(event) {
+  document.addEventListener("click", function(event) {
     let target = event.target;
-    let targetClasses = target.className.split(' ');
-    let htmlOperators = document.querySelectorAll('.operator');
+    let targetClasses = target.className.split(" ");
+    let htmlOperators = document.querySelectorAll(".operator");
     let operators = Array.from(htmlOperators);
     operators = operators.slice(1, operators.length - 1);
 
-    // append only one decimal per screen session
-    if(targetClasses[1] === 'decimal' && !decimalPressed) {
-      decimalPressed = true;
-      screen.innerText += target.innerText;
-    }
-    else if(targetClasses[1] === 'number' || targetClasses[1] === 'decimal') {
-      // if the screen's inner-text is '0' and the button pressed is a number then clear screen to allow number to append without a leading '0'
-      if(screen.innerText === '0' && targetClasses[1] === 'number' || operatorPressed) {
-        screen.innerText = '';
-      }
-      if((targetClasses[1] === 'decimal' && !decimalPressed) || (targetClasses[1] === 'number' && screen.innerText !== '0')) {
+    console.log("target", target);
+
+    if (screen.innerText !== "Error") {
+      // append only one decimal per screen session
+      if (targetClasses[1] === "decimal" && !decimalPressed) {
+        decimalPressed = true;
+        if (calculated || operatorPressed) {
+          screen.innerText = `0${target.innerText}`;
+          calculated = false;
+        } else {
+          screen.innerText += target.innerText;
+        }
+      } else if (targetClasses[1] === "number") {
+        // if the screen's inner-text is '0' and the button pressed is a number then clear screen to allow number to append without a leading '0'
+        if (
+          (screen.innerText === "0" && targetClasses[1] === "number") ||
+          calculated ||
+          operatorPressed
+        ) {
+          console.log("clearing screen");
+          screen.innerText = "";
+          calculated = false;
+          operatorPressed = false;
+          resetOperators(operators);
+        }
+
         screen.innerText += target.innerText;
       }
-    }
-    // highlight selected operator to indicate an operation is being done with that operator
-    else if(targetClasses[1] === 'operator' && targetClasses[2] !== 'clear' && targetClasses[2] !== 'equals') {
-      operatorPressed = true;
-      resetOperators(operators);
+      // highlight selected operator to indicate an operation is being done with that operator
+      else if (
+        targetClasses[1] === "operator" &&
+        targetClasses[2] !== "clear" &&
+        targetClasses[2] !== "equals"
+      ) {
+        calculated = false;
+        operatorPressed = true;
+        decimalPressed = false;
+        resetOperators(operators);
 
-      target.style.background = 'white';
-      target.style.color = '#ffa500';
+        target.style.background = "white";
+        target.style.color = "#ffa500";
 
-      numArray[0] = Number(screen.innerText);
-      selectedOperator = targetClasses[2];
+        numArray[0] = Number(screen.innerText);
+        selectedOperator = targetClasses[2];
+      }
+      // calculate when button pressed has a class of 'equals'
+      else if (targetClasses[2] === "equals") {
+        if (calculated) {
+          numArray[0] = Number(screen.innerText);
+        } else {
+          numArray[1] = Number(screen.innerText);
+        }
+
+        if (selectedOperator !== undefined) {
+          console.log("numArray", numArray);
+          let result = calculate(numArray, selectedOperator);
+          resetOperators(operators);
+          screen.innerText = result;
+          operatorPressed = false;
+          calculated = true;
+        }
+      }
     }
     // clear screen if the button pressed has a class of 'clear'
-    else if(targetClasses[2] === 'clear') {
+    if (targetClasses[2] === "clear") {
       resetOperators(operators);
+      numArray = [NaN, NaN];
       decimalPressed = false;
       operatorPressed = false;
+      calculated = false;
       selectedOperator = null;
       screen.innerText = 0;
     }
-    // calculate when button pressed has a class of 'equals'
-    else if(targetClasses[2] === 'equals') {
-      if(operatorPressed) {
-        numArray[1] = Number(screen.innerText);
-      } else {
-        numArray[0] = Number(screen.innerText);
-      }
-
-      let result = calculate(numArray, selectedOperator);
-      resetOperators(operators);
-      screen.innerText = result;
-      operatorPressed = false;
-    }
-  });
-}
-
-function resetOperators(operators) {
-  operators.forEach(function(operator){
-    operator.style.background = '#ffa500';
-    operator.style.color = 'white';
   });
 }
